abea3a9efa357d89711f0d78dd5c320752ad0266
Moritz Dietsche <moritz.dietsche@haako.io>
2016-06-01 09:44:46 +0200

    Empty project.

---
+++ MDNotificationView/MDNotificationView.swift
@@ -0,0 +1,7 @@
+//
+//  MDNotificationView.swift
+//
+//  Created by Moritz Dietsche on 01.06.16.
+//
+
+import Foundation
---
+++ MDNotificationViewDemo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MDNotificationViewDemo
+//
+//  Created by Moritz Dietsche on 01.06.16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
